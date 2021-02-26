ea642bd372b49d853aa92e5c1ece75a787025c9f
JohnMohr <74803311+JohnMohr@users.noreply.github.com>
2021-02-26 10:34:13 -0800

    debugging 2. I can't figure out why my database is loading but not posting on the page.

--- public/assets/js/burger_style.js
+++ public/assets/js/burger_style.js
@@ -1,61 +1,69 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
-    $(".change-devoured").on("click", function(event) {
-      var id = $(this).data("id");
-      var newDevoured = $(this).data("newDevoured");
-  
-      var newDevouredState = {
-        devoured: newDevoured
-      };
-  
-      // Send the PUT request.
-      $.ajax("/api/burgers/" + id, {
-        type: "PUT",
-        data: newDevouredState
-      }).then(
-        function() {
-          console.log("changed devoured to", newDevoured);
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
-  
-    $(".create-form").on("submit", function(event) {
-      // Make sure to preventDefault on a submit event.
+$(function () {
+  // Create New Burger
+  // Add a Burger form
+  $(".create-form").on("submit", function(event) {
       event.preventDefault();
-  
-      var newBurger = {
-        burger_name: $("#bu").val().trim(),
-        devoured: $("[name=devoured]:checked").val().trim()
+
+      let newBurger = {
+          // Value for burger_name column is id="burger" textarea
+          burger_name: $("#burger").val().trim(),
+          // Default devoured to false
+          devoured: 0
       };
-  
+
       // Send the POST request.
       $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger
+          type: "POST",
+          data: newBurger
       }).then(
-        function() {
-          console.log("cooked a new burger");
-          // Reload the page to get the updated list
-          location.reload();
-        }
+          function() {
+              console.log("Created new burger");
+              // Reload the page to get the updated list
+              location.reload();
+          }
       );
-    });
-  
-    $(".delete-burger").on("click", function(event) {
-      var id = $(this).data("id");
+  });
+
+  // DEVOURED or EAT ME! button
+  $(".change-devoured").on("click", function (event) {
+      let id = $(this).data("id");
+      console.log(id);
+      // Return value for data-devoured
+      let newBurger = $(this).data("devoured");
+      console.log("Current burger state: ", newBurger);
+
+      // Change to opposite devoured state
+      let newBurgerState = {
+          devoured: !newBurger
+      };
+
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+          type: "PUT",
+          data: newBurgerState
+      }).then(
+          function () {
+              console.log("Changed devoured to", newBurgerState);
+              // Reload the page to get the updated list
+              location.reload();
+          }
+      );
+  });
+
+  // Delete button
+  $(".delete-burger").on("click", function(event) {
+      let id = $(this).data("id");
   
       // Send the DELETE request.
       $.ajax("/api/burgers/" + id, {
         type: "DELETE"
       }).then(
         function() {
-          console.log("deleted burger", id);
+          console.log("Deleted burger", id);
           // Reload the page to get the updated list
           location.reload();
         }
       );
     });
-  });
-  
+});
