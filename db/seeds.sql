be7d1c98e243519b4f665fcc9d49b13d80d9fc22
JohnMohr <74803311+JohnMohr@users.noreply.github.com>
2021-02-26 11:38:31 -0800

    styling

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
--- INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
--- INSERT INTO burgers (burger_name) VALUES ('Mushroom Swiss');
--- INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
--- INSERT INTO burgers (burger_name, devoured) VALUES ('Lamb Burger', true);
--- INSERT INTO burgers (burger_name, devoured) VALUES ('Western BBQ Burger', true);
--- INSERT INTO burgers (burger_name) VALUES ('Spicy Chicken Fried Sandwich');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Swiss');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Lamb Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Western BBQ Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Spicy Chicken Fried Sandwich');
