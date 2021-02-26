3e700d12ebab284f83c3d98b5363abc70d15611d
JohnMohr <74803311+JohnMohr@users.noreply.github.com>
2021-02-26 12:26:34 -0800

    trying to fix heroku deploy by making db.sql instead of seed and schema

--- db/seeds.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
-INSERT INTO burgers (burger_name) VALUES ('Mushroom Swiss');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Lamb Burger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Western BBQ Burger', true);
-INSERT INTO burgers (burger_name) VALUES ('Spicy Chicken Fried Sandwich');
